64eed70a52bc2bb6d347c6f6a9aedd2ad34d90a4
SamihaSaha <Samiha.saha18@gmail.com>
2025-06-27 15:29:58 +0530

    updation of files done

--- test/src/app/app.ts
+++ test/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./pages/register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ test/src/app/pages/register/register.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    ReactiveFormsModule // ✅ And include it here
+  ],
+  templateUrl: './register.html',
+  styleUrls: ['./register.scss']
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      alert('Account created!');
+    }
+  }
+}
