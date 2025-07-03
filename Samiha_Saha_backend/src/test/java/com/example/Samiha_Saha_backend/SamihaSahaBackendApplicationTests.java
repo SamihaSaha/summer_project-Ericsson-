cc4329b3255720e327ac6ddd20f5f59097dc7b44
SamihaSaha <Samiha.saha18@gmail.com>
2025-07-03 13:40:08 +0530

    file name changed

---
+++ Samiha_Saha_backend/src/test/java/com/example/Samiha_Saha_backend/SamihaSahaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Samiha_Saha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamihaSahaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
